07e9203ac4052b2aca67a479137aa29440cee83f
Kruhliakou Dzmitry <kruglyakovdmitriy@mail.ru>
2020-10-20 21:14:35 +0300

    feat: implements extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  if (str == null) {
+    options.addition = 'null';
+  }
+
+  if (options.addition == undefined) {
+    options.addition = '';
+  } else if (options.addition == null) {
+    options.addition = 'null';
+  }
+
+  let repeatAddition = new Array(options.additionRepeatTimes)
+      .fill(options.addition)
+      .join(options.additionSeparator ? options.additionSeparator : '|');
+  let result = new Array(options.repeatTimes)
+      .fill(str + repeatAddition)
+      .join(options.separator ? options.separator : '+');
+  return result;
 };
-  
