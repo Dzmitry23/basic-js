1f125113dfa5ff58a13bcd91e2ee01ccc509c53e
Kruhliakou Dzmitry <kruglyakovdmitriy@mail.ru>
2020-10-20 21:13:35 +0300

    feat: implements transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,45 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let resArr = [];
+
+  if(Array.isArray(arr)) {
+    if(arr.length !== 0) {
+      for(let i = 0; i < arr.length; i++) {
+        switch(arr[i]) {
+          case '--discard-next':
+            if(arr[i + 2] === '--double-prev' || arr[i + 2] === '--discard-prev') {
+              i += 2;
+            } else {
+              i++;
+            }
+            break;
+          case '--discard-prev':
+            if(i > 0) {
+              resArr.pop();
+            }
+            break;
+          case '--double-next':
+            if(i < arr.length - 1) {
+              resArr.push(arr[i + 1]);
+            }
+            break;
+          case '--double-prev':
+            if(i > 0) {
+              resArr.push(arr[i - 1]);
+            }
+            break;
+          default:
+            resArr.push(arr[i]);
+            break
+        }
+      }
+    } else {
+      return arr;
+    }
+  } else {
+    throw new Error;
+  }
+
+  return resArr;
 };
