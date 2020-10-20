e01595a35aaa9dfdae238aa2f4e0a32618f468e0
Kruhliakou Dzmitry <kruglyakovdmitriy@mail.ru>
2020-10-20 21:10:39 +0300

    feat: implements cardon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,14 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if(typeof(sampleActivity) != 'string') {
+    return false;
+  }
+  let sampAct = parseFloat(sampleActivity);
+
+  if(sampAct <= 0 || sampAct > MODERN_ACTIVITY || !isFinite(sampAct)) {
+    return false;
+  }
+  return Math.ceil(Math.log(MODERN_ACTIVITY / sampAct) / (0.693 / HALF_LIFE_PERIOD));
 };
