e4cbf143293358e59956345f4be7b58346fc6128
Kruhliakou Dzmitry <kruglyakovdmitriy@mail.ru>
2020-10-20 21:14:01 +0300

    feat: implements simple-chain.js

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,44 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  values : [],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.values.length;
   },
+
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (value == null) {
+      value = 'null';
+    } else if (value == undefined) {
+      value = '';
+    }
+    this.values.push(value);
+    return this;
   },
+
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if(typeof(position) != 'number' || position < 1 || position > this.values.length - 1) {
+      this.values = [];
+      throw new Error;
+    } else {
+      this.values.splice(position - 1, 1);
+      return this;
+    }
   },
+
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.values = this.values.reverse();
+    return this;
   },
+
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    result = this.values.map(value => '( ' + value + ' )').join('~~');
+    this.values = [];
+    return result;
   }
 };
 
 module.exports = chainMaker;
+
+console.log(chainMaker.addLink('8.963').reverseChain().reverseChain().reverseChain().reverseChain().addLink({0: 'first', 1: 'second', 'length': 2}).reverseChain().addLink(3.14).addLink('DEF').reverseChain().finishChain());
